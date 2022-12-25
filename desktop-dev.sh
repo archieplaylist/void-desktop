aa67e91eac801b6ad22dc90186d8532f4f25e72f
Archie Tani <mario.tani25@gmail.com>
2022-12-25 14:45:01 +0700

    config up

--- desktop-dev.sh
+++ desktop-dev.sh
@@ -337,11 +337,7 @@ if [[ $REPLY = "yes" || $REPLY = "y" || $REPLY = "Y" || $REPLY = "Yes" || $REPLY
 
         echo -e "${LBLUE}Now I'll install OPENBOX(pkg)"
         echo -e "${LMAGENTA}----------------------------------------"
-        xbps-install openbox xorg-minimal xdg-user-dirs xorg xorg-fonts xfce4-terminal lxappearance-obconf obmenu-generator	 pcmanfm nitrogen tint2 -y
-        git clone https://github.com/ArchieTani/openbox-theme-collections /home/mario/.themes
-        git clone https://github.com/ArchieTani/tint2-theme-collections /home/mario/.config/tint2 --depth 1
-        mkdir -p /home/mario/.config/openbox
-        cp wm/autostart /home/mario/.config/openbox/
+        xbps-install openbox xorg-minimal xdg-user-dirs xdg-desktop-portal-gtk xdg-utils xorg xorg-fonts xfce4-terminal obconf obmenu-generator	 pcmanfm nitrogen tint2 -y
         echo -e "----------------------------------------${NORMAL}"
 
         echo -e "${YELLOW}Do you want to install lxdm? ${NORMAL}[${GREEN}Y${NORMAL}/${RED}n${NORMAL}]"
@@ -367,6 +363,9 @@ if [[ $REPLY = "yes" || $REPLY = "y" || $REPLY = "Y" || $REPLY = "Yes" || $REPLY
             echo -e "----------------------------------------${NORMAL}"
 
         fi
+
+        echo -e "${LBLUE}Please run ./wm/openbox/theme.sh"
+
     #========================================================
 
 
